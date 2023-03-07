a26bf562460f8a75da9f9ab83a6f8b86bf627a29
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-03-07 10:43:50 -0500

    Update SCALE_Input_Multithread.sh

--- input_shells/SCALE_Input_Multithread.sh
+++ input_shells/SCALE_Input_Multithread.sh
@@ -3,16 +3,13 @@
 #PBS -V
 #PBS -l nodes=1:ppn=8
 
-module load openmpi/2.1.6
+module load mpi
 module load scale/6.3.b12
-
-cat ${PBS_NODEFILE}
-#NP=$(grep -c node ${PBS_NODEFILE})
 
 cd $PBS_O_WORKDIR
 TMPDIR=/tmp/$USER/scale.$$
 
 #echo $NP
-scalerte -m -N 8 -M ${PBS_NODEFILE} -T $TMPDIR %%%file_name%%%
+scalerte -m -I 8 -M ${PBS_NODEFILE} -T $TMPDIR %%%file_name%%%
 
 rm -rf $TMPDIR
